d3b73c8404c6af0a9c8d156b4b69e0a86e6305b8
Harsh Patle <harshpatle491@gmail.com>
2024-12-21 23:46:10 +0530

    writing signup

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,8 +1,18 @@
 import User from "../models/user.model.js"
 
 export const signup = async(req,res) =>{
-    const 
+    const {fullName, email, password} = req.body;
     try{
+
+        if(password.length < 6){
+            return res.status(400).json({message: "Password must be at leat 6 characters "});
+        }
+
+        const user = await User.findOne({email});
+
+        if(user) return res.status(400).json({message:"Email Already Exists "});
+
+     }catch(error){
 
      }
 };
