a77d1754499d2c5c959ddd5e7d6d3b536e67e7f4
Harsh Patle <harshpatle491@gmail.com>
2024-12-21 23:40:52 +0530

    writing signup

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,29 @@
+import User from "../models/user.model.js"
+
+export const signup = async(req,res) =>{
+    const 
+    try{
+
+     }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+export const login = (req,res) =>{
+    res.send("login route")
+}
+
+export const logout = (req,res) =>{
+    res.send("Logout route ");
+}
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,21 @@
 import express  from "express";
+import dotenv from "dotenv";
 
-import autoRoutes from "../routes/auth.route.js";
+ import autoRoutes from "../routes/auth.route.js";
+import { connectDB } from "../lib/db.js";
+
+
+dotenv.config();
 
 const app = express();
 
+
 app.use("/api/auth", autoRoutes);
 
-app.listen(5001, () =>{
-    console.log("Server is running on port 5001");
+
+const PORT = process.env.PORT;
+app.listen(PORT, () =>{
+    console.log("Server is running on port: " +PORT );
+    connectDB()
 })
 
