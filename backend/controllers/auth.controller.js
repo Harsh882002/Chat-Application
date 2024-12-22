b1754194c6f2f3307fe31ce008a5ff1ab3a1347d
Harsh Patle <harshpatle491@gmail.com>
2024-12-23 01:01:51 +0530

    signup complete

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,13 +1,17 @@
+import { generateToken } from "../lib/utils.js";
 import User from "../models/user.model.js";
 import bcrypt from "bcrypt";
 
 export const signup = async (req, res) => {
   const { fullName, email, password } = req.body;
   try {
+
+     if(!fullName || !email || !password){
+      res.status(400).json({message: "All Fields Are Required.."})
+     }
+    
     if (password.length < 6) {
-      return res
-        .status(400)
-        .json({ message: "Password must be at leat 6 characters " });
+      return res.status(400).json({ message: "Password must be at leat 6 characters " });
     }
 
     const user = await User.findOne({ email });
@@ -23,7 +27,17 @@ export const signup = async (req, res) => {
       password: hashedPassword,
     });
 
-    if (newUser) {   
+    if (newUser) {
+      //generate jwt token here
+      generateToken(newUser._id, res);
+      await newUser.save();
+
+      res.status(201).json({
+        _id: newUser._id,
+        fullName: newUser.fullName,
+        email: newUser.email,
+        profilePic: newUser.profilePic,
+      });
     } else {
       res.status(400).json({ message: "Invalid   User Data " });
     }
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,21 +1,26 @@
 import express  from "express";
 import dotenv from "dotenv";
+dotenv.config();
 
  import autoRoutes from "../routes/auth.route.js";
 import { connectDB } from "../lib/db.js";
 
-
-dotenv.config();
-
 const app = express();
+app.use(express.json()); // Parse JSON requests
 
+const result  = dotenv.config();
+
+if(result.error){
+    console.log("Failed to Load .env file", result.error);
+} 
 
 app.use("/api/auth", autoRoutes);
 
+ 
 
-const PORT = process.env.PORT;
-app.listen(PORT, () =>{
-    console.log("Server is running on port: " +PORT );
-    connectDB()
+const PORT = process.env.PORT || 5001;
+app.listen(PORT, async() =>{
+    console.log("Server is running on port: " + PORT );
+   await connectDB()
 })
 
