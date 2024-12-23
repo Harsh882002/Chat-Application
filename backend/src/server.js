b072f75927421039a694bbe28925d0f5fbf0497c
Harsh Patle <harshpatle491@gmail.com>
2024-12-23 16:34:22 +0530

    new data

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,26 +1,26 @@
-import express  from "express";
+import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 dotenv.config();
 
- import autoRoutes from "../routes/auth.route.js";
+import autoRoutes from "../routes/auth.route.js";
 import { connectDB } from "../lib/db.js";
 
 const app = express();
 app.use(express.json()); // Parse JSON requests
+app.use(cookieParser());
 
-const result  = dotenv.config();
 
-if(result.error){
-    console.log("Failed to Load .env file", result.error);
-} 
+const result = dotenv.config();
+
+if (result.error) {
+  console.log("Failed to Load .env file", result.error);
+}
 
 app.use("/api/auth", autoRoutes);
 
- 
-
 const PORT = process.env.PORT || 5001;
-app.listen(PORT, async() =>{
-    console.log("Server is running on port: " + PORT );
-   await connectDB()
-})
-
+app.listen(PORT, async () => {
+  console.log("Server is running on port: " + PORT);
+  await connectDB();
+});
