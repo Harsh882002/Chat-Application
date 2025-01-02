6b115bf83db1dd3660dbe36d2234c286d380d921
Harsh Patle <harshpatle491@gmail.com>
2025-01-02 13:23:43 +0530

    working on profile page

--- frontend/src/pages/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useAuthStore } from '../store/useAuthStore';
 
 const ProfilePage = () => {
+
+const {authUser, isUpdatingProfile, updateProfile} = useAuthStore();
+  const [selectedImg , setSelectedImg] = useState(null);
+
   return (
-    <div>
-      <h1>ProfilePage</h1>
+    <div className='h-screen pt-20'>
+       <div className='max-w-2xl mx-auto p-4 py-8'>
+        <div className='bg-base-200 rounded-xl p-6 space-y-8'>
+          <div className='text-center'>
+            <h1 className='text-2xl font-semibold'>Profile</h1>
+            <p>Your Profile Information</p>
+          </div>
+
+          {/* avatar upload section  */}
+          <div className='flex flex-col items-center gap-4'>
+            <div className='relative'>
+              <img src= {selectedImg || authUser.profilepic || "avatar.png"}
+              alt="profile" 
+              className='size-32 rounded-full object-cover border-4'
+              />
+            </div>
+          </div>
+        </div>
+       </div>
     </div>
   )
 }
