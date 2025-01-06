d740c09e445bb0ddf78fbebdd6664e73761f627e
Harsh Patle <harshpatle491@gmail.com>
2025-01-06 17:07:01 +0530

    sidebar complete

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,10 +2,10 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
- dotenv.config();
+dotenv.config();
 
 import autoRoutes from "../routes/auth.route.js";
-import messageRoute from "../routes/message.route.js"
+import messageRoute from "../routes/message.route.js";
 import { connectDB } from "../lib/db.js";
 
 const app = express();
@@ -14,12 +14,13 @@ app.use(cookieParser());
 
 app.use(
   cors({
-  origin: "http://localhost:5173",
-  credentials: true, 
-}));
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
 app.use("/api/auth", autoRoutes);
-app.use("/api/message", messageRoute);
+app.use("/api/messages", messageRoute);
 
 const result = dotenv.config();
 
@@ -27,11 +28,8 @@ if (result.error) {
   console.log("Failed to Load .env file", result.error);
 }
 
-
-
 const PORT = process.env.PORT || 5001;
 app.listen(PORT, async () => {
   console.log("Server is running on port: " + PORT);
   await connectDB();
 });
-
